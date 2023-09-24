cab4c3a56faa8dc7febbc3557b687fcc4c1a60ad
AetherealDev <131472446+AetherealDev@users.noreply.github.com>
2023-09-24 18:15:54 -0400

    added the contact page, complete with form validation and bootstrap styling

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,89 @@
+import {useEffect} from "react";
+
 export default function Contact() {
+    useEffect(() => {
+        // Initialize form validation
+        const form = document.getElementById("contact-form");
+        form.addEventListener("submit", function (event) {
+            if (!form.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add("was-validated");
+        });
+    }, []);
+
     return (
-        <div>
+        <>
             <h1>Contact</h1>
-            <p>Phone: 220-204-8949</p>
-            <p>Email: ejsiewny@gmail.com</p>
-        </div>
-    )
+            <div className="container">
+                <form id="contact-form" noValidate>
+                    {/* Add the "noValidate" attribute to prevent browser validation */}
+                    <div className="mb-3">
+                        <label className="form-label">Name</label>
+                        <input
+                            className="form-control"
+                            id="name"
+                            type="text"
+                            placeholder="Name"
+                            required
+                        />
+                        <div className="invalid-feedback">
+                            Please provide your name.
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label" htmlFor="emailAddress">
+                            Email Address
+                        </label>
+                        <input
+                            className="form-control"
+                            id="emailAddress"
+                            type="email"
+                            placeholder="Email Address"
+                            required
+                        />
+                        <div className="invalid-feedback">
+                            Please provide a valid email address.
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label" htmlFor="phoneNumber">
+                            Phone Number
+                        </label>
+                        <input
+                            className="form-control"
+                            id="phoneNumber"
+                            type="tel"
+                            placeholder="Phone Number"
+                            required
+                        />
+                        <div className="invalid-feedback">
+                            Please provide your phone number.
+                        </div>
+                    </div>
+
+                    <div className="d-none" id="submitSuccessMessage">
+                        <div className="text-center mb-3">Form submission successful!</div>
+                    </div>
+
+                    <div className="d-none" id="submitErrorMessage">
+                        <div className="text-center text-danger mb-3">
+                            Error sending message!
+                        </div>
+                    </div>
+
+                    <div className="d-grid">
+                        <button
+                            className="btn btn-primary btn-lg"
+                            id="submitButton"
+                            type="submit"
+                        >
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </>
+    );
 }
