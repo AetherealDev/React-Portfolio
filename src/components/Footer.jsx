b859bc2c3555f6862d2d9fdc69c5d8ec89a15171
AetherealDev <131472446+AetherealDev@users.noreply.github.com>
2023-09-24 19:32:04 -0400

    added resume using react-pdf

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faGithub} from "@fortawesome/free-brands-svg-icons";
+import {faLinkedin} from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
 
     const Name = 'Ethan Siewny'; // Replace with your actual name
     const githubLink = 'https://github.com/AetherealDev'; // Replace with your GitHub profile URL
+    const linkedInLink = 'https://www.linkedin.com/in/ethan-siewny-604568257/'; // Replace with your LinkedIn profile URL
 
 
     return (
@@ -16,6 +18,11 @@ export default function Footer() {
             <button className="btn btn-dark">
                 <a href={githubLink}  rel="noopener noreferrer">
                     <FontAwesomeIcon icon={faGithub} /> GitHub
+                </a>
+            </button>
+            <button className="btn btn-dark">
+                <a href={linkedInLink}  rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
                 </a>
             </button>
         </footer>
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,5 +1,38 @@
+import ResumeDoc from '../assets/Resume.pdf';
+import { Document, Page } from 'react-pdf';
+import { pdfjs } from 'react-pdf';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
+
 export default function Resume() {
+
+    pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+        'pdfjs-dist/build/pdf.worker.min.js',
+        import.meta.url,
+    ).toString();
+
+
+    const pdfUrl = 'https://cdn1.frocdn.ch/rrhLKfaDXssAKUd.pdf';
+
+    const handleDownload = () => {
+        // Create an invisible anchor element and trigger a click event to start the download.
+        const link = document.createElement('a');
+        link.href = pdfUrl;
+        link.download = 'resume.pdf';
+        link.target = '_blank'; // Opens the link in a new tab
+        link.style.display = 'none';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
+
     return (
-        <></>
-    )
+        <div style={{ width: '25%', height: '50%' }}>
+            <Document file={ResumeDoc}>
+                <Page pageNumber={1} />
+            </Document>
+            <button className="btn btn-primary" onClick={handleDownload}>Download Resume</button>
+        </div>
+    );
 }
